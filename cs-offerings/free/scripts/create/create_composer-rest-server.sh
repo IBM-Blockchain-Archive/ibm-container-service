b63edef0f1ab4fde312c2f66a63bf0d651d689d1
Simon Stone <sstone1@uk.ibm.com>
2017-08-31 21:07:38 +0100

    Composer REST server now requires business network name

--- cs-offerings/free/scripts/create/create_composer-rest-server.sh
+++ cs-offerings/free/scripts/create/create_composer-rest-server.sh
@@ -8,6 +8,15 @@ else
     echo "Please run the script from 'scripts' or 'scripts/create' folder"
 fi
 
+COMPOSER_BUSINESS_NETWORK=$1
+if [ -z ${COMPOSER_BUSINESS_NETWORK} ]; then
+	echo "Usage: $0 <businessNetworkId>"
+    exit 1
+fi
+
+echo "Preparing yaml file for create composer-rest-server"
+sed -e "s/%COMPOSER_BUSINESS_NETWORK%/${COMPOSER_BUSINESS_NETWORK}/g" ${KUBECONFIG_FOLDER}/composer-rest-server.yaml.base > ${KUBECONFIG_FOLDER}/composer-rest-server.yaml
+
 echo "Creating composer-rest-server pod"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/composer-rest-server.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/composer-rest-server.yaml
