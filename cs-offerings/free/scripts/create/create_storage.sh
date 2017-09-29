a381ae41efcc7c53a586a23d0c759f312132feba
dhyey <dbshah@ibm.com>
2017-09-29 14:45:40 -0400

    adding parse args to storage script

--- cs-offerings/free/scripts/create/create_storage.sh
+++ cs-offerings/free/scripts/create/create_storage.sh
@@ -8,8 +8,35 @@ else
     echo "Please run the script from 'scripts' or 'scripts/create' folder"
 fi
 
+WITH_COUCHDB=false
+PAID=false
+
+Parse_Arguments() {
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--with-couchdb)
+				echo "Configured to setup network with couchdb"
+				WITH_COUCHDB=true
+				;;
+			--paid)
+				echo "Configured to setup a paid storage on ibm-cs"
+				PAID=true
+				;;
+		esac
+		shift
+	done
+}
+
+Parse_Arguments $@
+
+if [ "${PAID}" == "true" ]; then
+	OFFERING="paid"
+else
+	OFFERING="free"
+fi
+
 echo "Creating Persistent Volumes"
-if [ "${1}" == "--paid" ]; then
+if [ "${PAID}" == "true" ]; then
 	if [ "$(kubectl get pvc | grep shared-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep composer-pvc | awk '{ print $2 }')" != "Bound" ]; then
 		echo "The paid PVC does not seem to exist"
 		echo "Creating PVC named shared-pvc and composer-pvc"
