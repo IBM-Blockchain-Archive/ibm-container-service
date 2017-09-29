ffd7b48c88c8c713bfd96880e14469f5f7fbf295
dhyey <dbshah@ibm.com>
2017-09-29 14:56:31 -0400

    adding parse args to delete scripts

--- cs-offerings/free/scripts/delete/delete_composer-rest-server.sh
+++ cs-offerings/free/scripts/delete/delete_composer-rest-server.sh
@@ -8,6 +8,33 @@ else
     echo "Please run the script from 'scripts' or 'scripts/delete' folder"
 fi
 
+WITH_COUCHDB=false
+PAID=false
+
+Parse_Arguments() {
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--with-couchdb)
+				echo "Configured to setup network with couchdb"
+				WITH_COUCHDB=true
+				;;
+			--paid)
+				echo "Configured to setup a paid storage on ibm-cs"
+				PAID=true
+				;;
+		esac
+		shift
+	done
+}
+
+Parse_Arguments $@
+
+if [ "${PAID}" == "true" ]; then
+	OFFERING="paid"
+else
+	OFFERING="free"
+fi
+
 echo "Deleting Existing Composer Rest Server pod"
 echo "Running: kubectl delete -f ../kube-configs/composer-rest-server.yaml"
 kubectl delete -f ../kube-configs/composer-rest-server.yaml
@@ -18,8 +45,8 @@ while [ "$(kubectl get deployments | grep composer-rest-server | wc -l | awk '{p
 done
 
 echo "Deleting Existing Composer Rest Server services"
-echo "Running: kubectl delete -f ${KUBECONFIG_FOLDER}/composer-rest-server-services.yaml"
-kubectl delete -f ${KUBECONFIG_FOLDER}/composer-rest-server-services.yaml
+echo "Running: kubectl delete -f ${KUBECONFIG_FOLDER}/composer-rest-server-services-${OFFERING}.yaml"
+kubectl delete -f ${KUBECONFIG_FOLDER}/composer-rest-server-services-${OFFERING}.yaml
 
 while [ "$(kubectl get svc | grep composer-rest-server | wc -l | awk '{print $1}')" != "0" ]; do
 	echo "Waiting for composer rest server to be deleted"
--- cs-offerings/free/scripts/delete/delete_storage.sh
+++ cs-offerings/free/scripts/delete/delete_storage.sh
@@ -8,6 +8,37 @@ else
     echo "Please run the script from 'scripts' or 'scripts/create' folder"
 fi
 
-echo "Deleting Persistant Storage"
-echo "Running: kubectl delete -f ${KUBECONFIG_FOLDER}/storage.yaml"
-kubectl delete -f ${KUBECONFIG_FOLDER}/storage.yaml
+DELETE_VOLUMES=false
+WITH_COUCHDB=false
+PAID=false
+
+Parse_Arguments() {
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--paid)
+				echo "Configured to setup a paid storage on ibm-cs"
+				PAID=true
+				;;
+			--include-volumes | -i)
+				DELETE_VOLUMES=true
+				;;
+		esac
+		shift
+	done
+}
+
+Parse_Arguments $@
+
+if [ "${PAID}" == "true" ]; then
+	OFFERING="paid"
+else
+	OFFERING="free"
+fi
+
+if [ "${DELETE_VOLUMES}" == "true" ]; then
+	echo "Deleting Persistant Storage"
+	echo "Running: kubectl delete -f ${KUBECONFIG_FOLDER}/storage-${OFFERING}.yaml"
+	kubectl delete -f ${KUBECONFIG_FOLDER}/storage-${OFFERING}.yaml
+else
+	"-i | --include-volumes not included in the command, will not delete storage/volumes."
+fi
