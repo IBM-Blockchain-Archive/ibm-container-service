cf846c3f24f78645c0ef9c4300d182b1ae36e839
dhyey <dbshah@ibm.com>
2017-09-29 10:10:05 -0400

    renaming paid pvc to match the normal one

--- cs-offerings/free/scripts/create/create_storage.sh
+++ cs-offerings/free/scripts/create/create_storage.sh
@@ -10,22 +10,23 @@ fi
 
 echo "Creating Persistent Volumes"
 if [ "${1}" == "--paid" ]; then
-	if [ "$(kubectl get pvc | grep shared-pvc-paid | wc -l | awk '{ print $1 }')" == "0" ]; then
+	if [ "$(kubectl get pvc | grep shared-pvc | wc -l | awk '{ print $1 }')" == "0" ]; then
 		echo "The paid PVC does not seem to exist"
-		echo "Creating PVC named shared-pvc-paid"
+		echo "Creating PVC named shared-pvc"
 
 		# making a PVC on ibm-cs paid version
 		echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage-paid.yaml"
 		kubectl create -f ${KUBECONFIG_FOLDER}/storage-paid.yaml
 		sleep 5
 
-		while [ "$(kubectl get pvc | grep shared-pvc-paid | wc -l | awk '{ print $1 }')" == "0" ];
+		while [ "$(kubectl get pvc | grep shared-pvc | wc -l | awk '{ print $1 }')" == "0" ];
 		do
 			echo "Waiting for storage to be created"
 			sleep 5
 		done
 	else
-		echo "The paid PVC exists, not creating again"
+		echo "The PVC with name shared-pvc exists, not creating again"
+		#echo "Note: This can be a normal storage and not a ibm-cs storage, please check for more details"
 	fi
 else
 	if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $3}')" != "shared-pv" ]; then
