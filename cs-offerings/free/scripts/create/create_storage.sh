756ff25d16e9c74a468845afd207e134e48868e8
dhyey <dbshah@ibm.com>
2017-09-29 10:06:37 -0400

    adding logic for paid in create_storage

--- cs-offerings/free/scripts/create/create_storage.sh
+++ cs-offerings/free/scripts/create/create_storage.sh
@@ -8,21 +8,42 @@ else
     echo "Please run the script from 'scripts' or 'scripts/create' folder"
 fi
 
-if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $3}')" != "shared-pv" ]; then
-    echo "The Persistant Volume does not seem to exist or is not bound"
-	echo "Creating Persistant Volume"
-	
-	# making a pv on kubernetes
-	echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage.yaml"
-    kubectl create -f ${KUBECONFIG_FOLDER}/storage.yaml
-	sleep 5
-	if [ "kubectl get pvc | grep shared-pvc | awk '{print $3}'" != "shared-pv" ]; then
-		echo "Success creating PV"
+echo "Creating Persistent Volumes"
+if [ "${1}" == "--paid" ]; then
+	if [ "$(kubectl get pvc | grep shared-pvc-paid | wc -l | awk '{ print $1 }')" == "0" ]; then
+		echo "The paid PVC does not seem to exist"
+		echo "Creating PVC named shared-pvc-paid"
+
+		# making a PVC on ibm-cs paid version
+		echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage-paid.yaml"
+		kubectl create -f ${KUBECONFIG_FOLDER}/storage-paid.yaml
+		sleep 5
+
+		while [ "$(kubectl get pvc | grep shared-pvc-paid | wc -l | awk '{ print $1 }')" == "0" ];
+		do
+			echo "Waiting for storage to be created"
+			sleep 5
+		done
 	else
-		echo "Failed to create PV"
+		echo "The paid PVC exists, not creating again"
 	fi
 else
-	echo "The Persistant Volume exists, not creating again"
-fi
+	if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $3}')" != "shared-pv" ]; then
+		echo "The Persistant Volume does not seem to exist or is not bound"
+		echo "Creating Persistant Volume"
+		
+		# making a pv on kubernetes
+		echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage.yaml"
+		kubectl create -f ${KUBECONFIG_FOLDER}/storage.yaml
+		sleep 5
+		if [ "kubectl get pvc | grep shared-pvc | awk '{print $3}'" != "shared-pv" ]; then
+			echo "Success creating PV"
+		else
+			echo "Failed to create PV"
+		fi
+	else
+		echo "The Persistant Volume exists, not creating again"
+	fi
 
+fi
 
