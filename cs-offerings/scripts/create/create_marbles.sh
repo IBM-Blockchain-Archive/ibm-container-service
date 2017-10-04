526f72c53ef802a02f3c2588c10745c4914a5048
dhyey <dbshah@ibm.com>
2017-10-04 09:48:50 -0400

    updating the marbles external ip

--- cs-offerings/scripts/create/create_marbles.sh
+++ cs-offerings/scripts/create/create_marbles.sh
@@ -13,7 +13,6 @@ Parse_Arguments() {
 	while [ $# -gt 0 ]; do
 		case $1 in
 			--paid)
-				echo "Configured to setup a paid storage on ibm-cs"
 				PAID=true
 				;;
 		esac
@@ -25,8 +24,10 @@ Parse_Arguments $@
 
 if [ "${PAID}" == "true" ]; then
 	OFFERING="paid"
+	PUBLIC_IP=$(kubectl get svc | grep marbles | awk '{print $3}' )
 else
 	OFFERING="free"
+	PUBLIC_IP=$(bx cs workers blockchain | awk 'FNR==3{print $2}')
 fi
 
 echo "Creating marbles service"
@@ -41,6 +42,5 @@ kubectl create -f ${KUBECONFIG_FOLDER}/marbles.yaml
 
 echo "Waiting for marbles to be up....."
 sleep 30
-
-PUBLIC_IP=$(bx cs workers blockchain | awk 'FNR==3{print $2}')
+echo "install and instantiate might take longer than expected"
 echo "Please go to Marbles UI: http://${PUBLIC_IP}:32001 for next steps"
